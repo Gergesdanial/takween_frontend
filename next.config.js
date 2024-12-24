de673e142b4c595737ae325e62d4ab364a21655e
Gerges Makarious Danial <127982294+Gergesdanial@users.noreply.github.com>
2024-12-24 18:15:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  eslint: {
+    // Ignore ESLint errors during the build process
+    ignoreDuringBuilds: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
