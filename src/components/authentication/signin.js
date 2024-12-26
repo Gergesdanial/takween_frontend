c83593d83605c5d5999399081c843e2a54d2d69d
Gerges Makarious Danial <127982294+Gergesdanial@users.noreply.github.com>
2024-12-26 15:49:29 +0200

    Update signin.js

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -86,7 +86,7 @@ export default function SignInComponent({ onClose }) {
 
               try {
                 const res = await AxiosWrapper.post(
-                  "https://50.19.124.30:8000/signin",
+                  "https://50.19.124.30/signin",
                   signInData,
                 );
                 setWrongInfoError(false);
